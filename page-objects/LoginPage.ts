bf4bd8785b184d658de72a6fcd8a14badbca280d
SmNizamani <62019584+SmNizamani@users.noreply.github.com>
2024-09-02 00:22:27 +0200

    Added Login Case

--- page-objects/LoginPage.ts
+++ page-objects/LoginPage.ts
@@ -2,32 +2,34 @@ import { expect, Locator, Page } from "@playwright/test";
 
 export class LoginPage {
     readonly page: Page
+    readonly loginBtn: Locator
     readonly usernameInput: Locator
     readonly passwordInput: Locator
-    readonly loginBtn: Locator
-    readonly errorMessage: Locator
+    readonly submitBtn: Locator
+    readonly titleText: Locator
 
     constructor(page: Page) {
         this.page = page
-        this.usernameInput = page.locator('#user-name')
-        this.passwordInput = page.locator('#password')
-        this.loginBtn = page.locator('#login-button')
-        this.errorMessage = page.locator('.error-message-container h3')
+        this.usernameInput = page.locator('#input-email')
+        this.passwordInput = page.locator('#input-password')
+        this.loginBtn = page.locator("(//a[@role='button'])[12]")
+        this.submitBtn = page.locator("input[value='Login']")
+        this.titleText = page.locator("(//h2[normalize-space()='My Account'])[1]")
     }
 
     async visit()
     {
-        await this.page.goto('https://the-internet.herokuapp.com/')
+        await this.page.goto('https://ecommerce-playground.lambdatest.io/')
     }
 
-    // async login(username: string, password: string) {
-    //     await this.usernameInput.fill(username)
-    //     await this.passwordInput.fill(password)
-    //     await this.loginBtn.click()
-        
-    // }
+    async loginWithCorrectCredentials() {
+        await this.loginBtn.click()
+        await this.usernameInput.fill('smnizamani@gmail.com')
+        await this.passwordInput.fill('123456')
+        await this.submitBtn.click()
+    }
 
-    // async assertErrorMessage() {
-    //     await expect(this.errorMessage).toContainText('Epic sadface: Username and password do not match any user in this service')
-    // }
+    async assertPageTitle() {
+        await expect(this.titleText).toContainText('My Account')
+    }
 }
--- tests/e2e-login.spec.ts
+++ tests/e2e-login.spec.ts
@@ -1,34 +1,22 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../page-objects/LoginPage";
-import { Navbar } from "../page-objects/Navbar";
 
 test.describe.only("Login and Logout Scnearios", () => {
     let loginPage:LoginPage
-    let navBar:Navbar
     //Go to link before each test case hook.
     test.beforeEach(async ({ page }) => {
         loginPage = new LoginPage(page)
-        navBar = new Navbar(page)
         await loginPage.visit()
-        await navBar.clickOnTab("login form")
-        await page.pause()
 
     }
     )
 
-    test('Negative case for login', async ({ page }) => {
+    test('login test', async ({ page }) => {
         // await loginPage.login('wrong', 'right')
         // await loginPage.assertErrorMessage()
-        console.log('first case')
-    }
-    )
-
-    test('Positive case for login', async ({ page }) => {
-        // await loginPage.login('standard_user', 'secret_sauce')
-        // const accSummaryTab = await page.locator('.title')
-        // await expect(accSummaryTab).toHaveText('Products')
-        // await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
-        console.log('first case')
+        await loginPage.loginWithCorrectCredentials()
+        await page.pause()
+        await loginPage.assertPageTitle()
     }
     )
 }
